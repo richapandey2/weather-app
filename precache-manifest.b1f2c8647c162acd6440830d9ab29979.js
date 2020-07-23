71216bfda98c991fd303cef13fa0157c206ce994
richapandey2 <47270918+richapandey2@users.noreply.github.com>
2020-07-23 16:06:04 +0530

    Updates

---
+++ precache-manifest.b1f2c8647c162acd6440830d9ab29979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799fd919c07da93c2b133eef76b72565",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6e1dda2017bc0d3b313c",
+    "url": "/weather-app/static/css/main.b8d30b13.chunk.css"
+  },
+  {
+    "revision": "1cc68fc33a6757b2a923",
+    "url": "/weather-app/static/js/2.c06bf234.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.c06bf234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e1dda2017bc0d3b313c",
+    "url": "/weather-app/static/js/main.6becd504.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
